016e829a1c967308c425ff32a477d539a6ebeb35
SergioKoh <77022065+SergioKoh@users.noreply.github.com>
2021-03-11 21:48:52 +0300

    Completed the changing window but also added 2 progress bars for timers and two progress bars for discarding chips.

--- board/board.py
+++ board/board.py
@@ -1,6 +1,9 @@
 import tkinter as tk
 from tkinter import ttk
+import time
 
+
+import main
 
 WIDTH, HEIGHT = 1200, 800
 WIDTH_MIN, HEIGHT_MIN = 600, 400
@@ -11,40 +14,41 @@ class WFrame(ttk.Frame):
     """Container for
     """
 
-    def __init__(self, master, style, width, height):
+    def __init__(self, master, width, height):
         super().__init__(master)
         self.master = master
-        self.style = style
+        self.style = ttk.Style()
+        self.style.configure('TFrame', borderwidth=3, relief=tk.RAISED, background='SaddleBrown')
         self.height = height
         self.width = width
-        self.width1 = int(0.067 * width)
-        self.width2 = int(0.366 * width)
-        self.width3 = int(0.067 * width)
-        self.width4 = int(0.067 * width)
-        self.width5 = int(0.366 * width)
-        self.width6 = int(0.067 * width)
+        self.width0 = int(0.017 * self.width)
+        self.width1 = int(0.466 * self.width)
+        self.width2 = int(0.017 * self.width)
+        self.width3 = int(0.017 * self.width)
+        self.width4 = int(0.466 * self.width)
+        self.width5 = width - self.width0 - self.width1 - self.width2 - self.width3 - self.width4
 
+        self.frame0 = ttk.Frame(self, style='TFrame', width=self.width0, height=self.height)
         self.frame1 = ttk.Frame(self, style='TFrame', width=self.width1, height=self.height)
         self.frame2 = ttk.Frame(self, style='TFrame', width=self.width2, height=self.height)
         self.frame3 = ttk.Frame(self, style='TFrame', width=self.width3, height=self.height)
         self.frame4 = ttk.Frame(self, style='TFrame', width=self.width4, height=self.height)
         self.frame5 = ttk.Frame(self, style='TFrame', width=self.width5, height=self.height)
-        self.frame6 = ttk.Frame(self, style='TFrame', width=self.width6, height=self.height)
-        """
-        self.frame1.pack(fill=tk.BOTH, expand=1, side=tk.LEFT)
-        self.frame2.pack(fill=tk.BOTH, expand=1, side=tk.LEFT)
-        self.frame3.pack(fill=tk.BOTH, expand=1, side=tk.LEFT)
-        self.frame4.pack(fill=tk.BOTH, expand=1, side=tk.LEFT)
-        self.frame5.pack(fill=tk.BOTH, expand=1, side=tk.LEFT)
-        self.frame6.pack(fill=tk.BOTH, expand=1, side=tk.LEFT)
 
-        """
-        self.frame1.place(relx=0.0,  rely=0.05, relwidth=0.067, relheight=0.9)
-        self.frame2.place(relx=0.067, rely=0.05, relwidth=0.366, relheight=0.9)
-        self.frame3.place(relx=0.433, rely=0.05, relwidth=0.067, relheight=0.9)
-        self.frame4.place(relx=0.5, rely=0.05, relwidth=0.067, relheight=0.9)
-        self.frame5.place(relx=0.567, rely=0.05, relwidth=0.366, relheight=0.9)
-        self.frame6.place(relx=0.0933, rely=0.05, relwidth=0.067, relheight=0.9)
+        self.frame0.grid(column=0, row=0, sticky="nsew")
+        self.frame1.grid(column=1, row=0, sticky="nsew")
+        self.frame2.grid(column=2, row=0, sticky="nsew")
+        self.frame3.grid(column=3, row=0, sticky="nsew")
+        self.frame4.grid(column=4, row=0, sticky="nsew")
+        self.frame5.grid(column=5, row=0, sticky="nsew")
+
+        tk.Grid.rowconfigure(self, index=0, weight=1)
+        tk.Grid.columnconfigure(self, index=1, weight=1)
+        tk.Grid.columnconfigure(self, index=4, weight=1)
+
+        self.orient = 'vertical'
+        Window.progress_bar(self.master, self.frame5, self.orient)
+
 
 
 class Window(tk.Tk):
@@ -58,6 +62,7 @@ class Window(tk.Tk):
         self.resizable(True, True)
         self.minsize(WIDTH_MIN, HEIGHT_MIN)
 
+
     def center_screen(self, w, h):
         """Centering the window."""
         x = abs((self.winfo_screenwidth() - w)) // 2
@@ -70,14 +75,45 @@ class Window(tk.Tk):
         self.style.configure('TFrame', borderwidth=3, relief=tk.RAISED, background='SaddleBrown')
         self.width = w
         self.height = h
-        self.height1 = self.height3 = int(0.05 * self.height)
-        self.height2 = self.height - self.height1 - self.height3
+        self.height0 = self.height2 = int(0.05 * self.height)
+        self.height1 = self.height - self.height0 - self.height2
 
-        self.frame1 = ttk.Frame(self, style='TFrame', width=self.width, height=self.height1)
-        self.frame2 = WFrame(self, style='TFrame', width=self.width, height=self.height2)
-        self.frame3 = ttk.Frame(self, style='TFrame', width=self.width, height=self.height3)
 
-        self.frame1.place(rely=0.00, relheight=0.05)
-        self.frame2.place(rely=0.05, relheight=0.9)
-        self.frame3.place(rely=0.95, relheight=0.05)
+        self.frame0 = ttk.Frame(self, style='TFrame', width=self.width)
+        self.frame1 = WFrame(self, width=self.width, height=self.height1)
+        self.frame2 = ttk.Frame(self, style='TFrame', width=self.width)
+
+        self.frame0.grid(column=0, row=0, sticky="nsew")
+        self.frame1.grid(column=0, row=1, sticky="nsew")
+        self.frame2.grid(column=0, row=2, sticky="nsew")
+
+
+        tk.Grid.columnconfigure(self, index=0, weight=1)
+        tk.Grid.rowconfigure(self, index=1, weight=1)
+
+        self.orient = 'horizontal'
+        self.progress_bar(self.frame0, self.orient)
+        self.progress_bar(self.frame2, self.orient)
+
+
+    def progress_bar(self, frame, orient):
+        """"""
+#        self.style.theme_use('default')
+        self.frame = frame
+        self.orient = orient
+        if self.orient == 'horizontal':
+            self.style.configure("black.Horizontal.TProgressbar", background='red')
+            self.bar_hor = ttk.Progressbar(frame, length=int(0.5 * WIDTH_MIN), orient='horizontal', style='black.Horizontal.TProgressbar')
+            self.bar_hor['value'] = 50
+            self.bar_hor.pack(anchor="center")
+        else:
+            self.style.configure("black.Vertical.TProgressbar", background='red')
+            self.ver_anchor = "n"
+            for i in range(2):
+                self.bar_ver = ttk.Progressbar(frame, length=int(0.45 * HEIGHT_MIN), orient='vertical', style="black.Vertical.TProgressbar")
+                self.bar_ver['value'] = 50
+                self.bar_ver.pack(expand=True, anchor=self.ver_anchor)
+                self.ver_anchor = "s"
+
+
 
